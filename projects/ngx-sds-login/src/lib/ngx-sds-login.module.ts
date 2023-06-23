7626e4017dd62f0f8a8254494f853ca797bdf6ad
yarid tellez <ytellez@soaint.com>
2023-06-23 15:30:41 -0500

    arreglo llamado multiples reducer

--- projects/ngx-sds-login/src/lib/ngx-sds-login.module.ts
+++ projects/ngx-sds-login/src/lib/ngx-sds-login.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { NgxSdsLoginComponent } from './ngx-sds-login.component';
 import { CommonModule } from '@angular/common';
-
-
+import { StoreModule } from '@ngrx/store';
+import { userReducer } from './state/user/user.reducer';
+import { menuReducer } from './state/menu/menu.reducer';
+const rootReducer = {
+  user: userReducer,
+  menu: menuReducer
+};
 
 @NgModule({
   declarations: [
     NgxSdsLoginComponent
   ],
   imports: [
-    CommonModule,       
+    CommonModule,
+    StoreModule.forRoot(rootReducer)
   ],
   exports: [
     NgxSdsLoginComponent
   ]
 })
-export class NgxSdsLoginModule { 
+export class NgxSdsLoginModule {
 }
